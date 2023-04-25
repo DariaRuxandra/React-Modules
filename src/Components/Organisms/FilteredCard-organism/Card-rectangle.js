11d678fd0f191cd874b16611ef8e57d1e2cdf71a
DariaRuxandra <dariaruxandra15@gmail.com>
2023-04-26 01:30:50 +0300

    Pictures from component 2 need adjustments + I have to finish the button in component 4

--- src/Components/Organisms/FilteredCard-organism/Card-rectangle.js
+++ src/Components/Organisms/FilteredCard-organism/Card-rectangle.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import './Card-rectangle.scss'
+import React from "react";
+import "./Card-rectangle.scss";
 
 export default function Card_rectangle(props) {
   return (
-    <div className="card-rectangle col-md-8" >
-        <div className="content">
-            <div className="text">
-                {props.text}
-            </div>
-            <a href="#" className="link">Meer informatie</a>
+    <div className="container">
+      <div className="row custom-row">
+        <div className="card-rectangle col-md-8">
+          <div className="content">
+            <div className="text">{props.text}</div>
+            <a href="#" className="link">
+              Meer informatie
+            </a>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
